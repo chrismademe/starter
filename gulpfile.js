30f570dfd8b4545a400aa05c777ef853562d5c3e
Chris Galbraith <chris@resknow.co.uk>
2020-01-31 10:04:49 +0000

    Add textarea & select components

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
+const playwright = require("playwright");
+const postcssPlugins = [require("tailwindcss"), require("autoprefixer")];
 
 const _ = {
     src: `./src`,
-    dest: `./assets`
+    dest: `./assets`,
+    browserTesting: {
+        url: `http://localhost:8080`,
+        screenshotDir: `testing`,
+        browsers: ["chromium", "firefox", "webkit"],
+        devices: ["iPad Mini", "iPhone 6", "iPhone SE"]
+    }
 };
 
 const compileCSS = function() {
-    let plugins = [require("tailwindcss"), require("autoprefixer")];
-
     return gulp
         .src(`${_.src}/css/style.css`)
-        .pipe(postcss(plugins))
+        .pipe(postcss(postcssPlugins))
         .pipe(gulp.dest(`${_.dest}/css`));
 };
 
@@ -22,6 +28,31 @@ const watch = function(done) {
     done();
 };
 
+const browserTest = function(done) {
+    let { browsers, devices, screenshotDir, url } = _.browserTesting;
+
+    // Create a screenshot for each browser
+    browsers.forEach(async browser => {
+        devices.forEach(async device => {
+            const currentDevice = playwright.devices[device];
+            const instance = await playwright[browser].launch();
+            const context = await instance.newContext({
+                viewport: currentDevice.viewport,
+                userAgent: currentDevice.userAgent
+            });
+            const page = await context.newPage(url);
+
+            await page.screenshot({
+                path: `./${screenshotDir}/${browser}-${device}.png`
+            });
+            await page.close();
+        });
+    });
+
+    done();
+};
+
 exports.default = compileCSS;
 exports.compileCSS = compileCSS;
+exports.browserTest = browserTest;
 exports.watch = gulp.parallel(watch);
